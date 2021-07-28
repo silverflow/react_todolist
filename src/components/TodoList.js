51f28eedb878687fb746e6fce26c9e995760175b
metafor <silveryflow@naver.com>
2021-07-29 02:31:30 +0900

    feat: add Toggle Function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,16 @@ import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
 import React from "react";
 
-const TodoList = ({ todos, onRemove }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
